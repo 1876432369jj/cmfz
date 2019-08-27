d3a43cd216cd1fc39bd443778c6067e8c491a381
1876432369jj <1876432369@qq.com>
2019-08-27 16:56:32 +0800

    sout 1.0

--- src/test/java/com/baizhi/Test.java
+++ src/test/java/com/baizhi/Test.java
@@ -3,5 +3,6 @@ package com.baizhi;
 public class Test {
     public static void main(String[] args) {
         System.out.println("大家好，我是蔡徐坤，丧坤的坤");
+        System.out.println("希望大家可以支持我的作品‘鸡你太美’");
     }
 }
