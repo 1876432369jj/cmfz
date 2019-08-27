142a8d9909d55ad21c25363c9d8892961de89886
1876432369jj <1876432369@qq.com>
2019-08-27 15:58:48 +0800

    init-cmfz

---
+++ src/main/java/com/baizhi/CmfzApplication.java
@@ -0,0 +1,13 @@
+package com.baizhi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmfzApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CmfzApplication.class, args);
+    }
+
+}
